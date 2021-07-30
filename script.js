fdcece1c2315acb19ca1853cdea59231ed83a7cd
Nandan Reddy <nandan.reddy1133@gmail.com>
2021-07-30 23:32:01 +0530

    Added a Message me section

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 //////////////////////////////////// DOM Elements ////////////////////////////////////
+const getElem = (elemId, root = document) => root.getElementById(elemId);
 const DOM = {
-	themeChangerButtonElement: document.getElementById('theme-changer'),
-	themeContainer: document.getElementById('theme'),
+	themeChangerButtonElement: getElem('theme-changer'),
+	themeContainer: getElem('theme'),
+	messageForm: getElem('message-form'),
 };
 
 ///////////////////////////////////// UI Methods /////////////////////////////////////
@@ -15,8 +17,84 @@ const UI = (function () {
 	};
 })();
 
+/////////////////////////////////////// Logic ////////////////////////////////////////
+const Utilities = (function () {
+	return {
+		isValidEmail(email) {
+			const re =
+				/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+			return re.test(String(email).toLowerCase());
+		},
+	};
+})();
+
+const Logic = (function () {
+	return {
+		messageForm: {
+			validate() {
+				const errors = [];
+				const messageForm = DOM.messageForm;
+				const formData = new FormData(messageForm);
+
+				const name = formData.get('name').trim();
+				if (!name) {
+					errors.push('Name is required.');
+				}
+				const email = formData.get('email').trim();
+				if (!email) {
+					errors.push('Email is required.');
+				} else if (Utilities.isValidEmail(email) === false) {
+					errors.push('Email is invalid.');
+				}
+				const message = formData.get('message');
+				if (!message) {
+					errors.push('Message is required.');
+				}
+				const formattedFormData = {
+					name,
+					email,
+					message,
+				};
+				const validationResult = {
+					isValid: errors.length === 0,
+					errors,
+					formattedFormData,
+				};
+				return validationResult;
+			},
+			sendMessage(messageObj) {
+				console.log(messageObj);
+				fetch('/', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/x-www-form-urlencoded',
+					},
+					body: new URLSearchParams(messageObj).toString(),
+				})
+					.then(() => console.log('Form successfully submitted'))
+					.catch((error) => alert(error));
+			},
+		},
+	};
+})();
+
 //////////////////////////////////// Controllers /////////////////////////////////////
 DOM.themeChangerButtonElement.addEventListener('click', (event) => {
 	// console.log('Clicked');
 	UI.switchTheme();
+});
+
+DOM.messageForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+	const {
+		isValid: isFormValid,
+		errors,
+		formattedFormData: formData,
+	} = Logic.messageForm.validate();
+
+	if (!isFormValid) {
+		return console.log(errors);
+	}
+	Logic.messageForm.sendMessage(formData);
+	console.log(formData);
 });
